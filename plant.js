4e61a71f05d8e61470767d5019832accb58f30b1
Franz Knupfer <franzknupfer@hotmail.com>
2019-10-28 16:53:06 -0700

    add reducer-like syntax

--- plant.js
+++ plant.js
@@ -3,14 +3,6 @@ import State from './state.js';
 // This may seem like the equivalent of a global variable but normally it would be injected into files where it is needed.
 let state = new State();
 
-// This is our function for creating new plants. It won't actually create new plants, though - instead, it needs to be passed into our update() function.
-const addPlant = inputtedName => {
-    name: inputtedName
-    light: 0
-    soil: 0
-    water: 0
-};
-
 const changeState = (prop) => {
   return (value) => {
     return (state) => ({
@@ -24,9 +16,29 @@ const feed = changeState("soil");
 const hydrate = changeState("water");
 const giveLight = changeState("light");
 
-const blueFood = changeState("soil")(5)
-const greenFood = changeState("soil")(10)
-const yuckyFood = changeState("soil")(-5)
+// This is our function for creating new plants. It won't actually create new plants, though - instead, it needs to be passed into our update() function.
+const addPlant = inputtedName => {
+  name: inputtedName
+  light: 0
+  soil: 0
+  water: 0
+};
 
-// To actually make updates to our store, we will call the following method:
-state.update(plant);
+const determineAction = (object, action) => {
+  switch(action) {
+    case "add":
+      return object;
+    case "feed":
+      return feed(object);
+    case "hydrate":
+      return hydrate(object);
+    case "giveLight":
+      return giveLight(object);
+  }
+}
+
+const updater = (object, action) => {
+  const newObject = determineAction(object, action);
+  return state.update(newObject);
+
+}
